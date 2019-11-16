669321fc95d07399fdb1d907097f2d8b3cb65fa4
Thorben <>
2019-11-16 23:25:01 +0100

    MinMaxHeap: delmax does not work

--- src/p2/datastructures/MinMaxHeap.java
+++ src/p2/datastructures/MinMaxHeap.java
@@ -1,48 +1,119 @@
 package p2.datastructures;
 
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.List;
 
-public class MinMaxHeap<Key extends Comparable <? super Key>> {
+public class MinMaxHeap<Key extends Comparable<? super Key>> {
 
-    public Key [] pq;
-    public int N = 0 ; // in pq [1..N] 0 wird nicht verwendet
+    public Key[] pq;
+    public int N = 0; // in pq [1..N] 0 wird nicht verwendet
 
     /***
      * @source AD_2 Sortieren 2 Fol. 31
      * @param maxN
      */
-    public MinMaxHeap (int maxN){
-        pq = (Key []) new Comparable [ maxN+1];
+    public MinMaxHeap(int maxN) {
+        pq = (Key[]) new Comparable[maxN + 1];
     }
 
-    public MinMaxHeap (List<Key> list){
-        pq = (Key []) new Comparable [ list.size()+1];
-        list.forEach(this::insert);
+    public MinMaxHeap(List<Key> list) {
+        //TODO Build method
     }
 
-    public boolean isIndexMinLevel(int i) {
+    /***
+     * source: https://en.wikipedia.org/wiki/Min-max_heap
+     */
+    private void floydBuildHeap(){
+        for(int i = N/2; i > 1; i--){
+            this.pushDown(i);
+        }
+    }
+
+    private boolean isIndexMinLevel(int i) {
         return (Math.log(i) % 2) == 0;
     }
 
-    public int getParentIndex(int i) {
+    private int getParentIndex(int i) {
         return i / 2;
     }
 
-    public int getGrantparentIndex(int i) {
+    private int getGrandparentIndex(int i) {
         return i / 4;
     }
 
-    public boolean hasGrandparent(int i) {
-        return this.getGrantparentIndex(i) > 0;
+    private boolean hasGrandparent(int i) {
+        return this.getGrandparentIndex(i) > 0;
     }
 
-    public int getLeftChildIndex(int i) {
+    private boolean isGrandChildOf(int grandchild, int grandparent) {
+        return this.getGrandparentIndex(grandchild) == grandparent;
+    }
+
+    private int getIndexOfLargestChildOrGrandChild(int index){
+        int leftChildIndex = this.getLeftChildIndex(index);
+        int rightChildIndex = this.getRightChildIndex(index);
+        int leftChildLeftChildIndex = this.getLeftChildIndex(leftChildIndex);
+        int rightChildLeftChildIndex = this.getRightChildIndex(leftChildIndex);
+        int leftChildRightChildIndex = this.getLeftChildIndex(rightChildIndex);
+        int rightChildRightChildIndex = this.getRightChildIndex(rightChildIndex);
+
+        int currentMinIndex = leftChildIndex;
+        if(rightChildIndex <= N && this.less(rightChildIndex,currentMinIndex)){
+            currentMinIndex = rightChildIndex;
+        }
+        if(leftChildLeftChildIndex <= N && this.less(leftChildLeftChildIndex,currentMinIndex)){
+            currentMinIndex = leftChildLeftChildIndex;
+        }
+        if(rightChildLeftChildIndex <= N && this.less(rightChildLeftChildIndex,currentMinIndex)){
+            currentMinIndex = rightChildLeftChildIndex;
+        }
+        if(leftChildRightChildIndex <= N && this.less(leftChildRightChildIndex,currentMinIndex)){
+            currentMinIndex = leftChildRightChildIndex;
+        }
+        if(rightChildRightChildIndex <= N && this.less(rightChildRightChildIndex,currentMinIndex)){
+            currentMinIndex = rightChildRightChildIndex;
+        }
+        return currentMinIndex;
+    }
+
+    private int getIndexOfSmallestChildOrGrandChild(int index) {
+        int leftChildIndex = this.getLeftChildIndex(index);
+        int rightChildIndex = this.getRightChildIndex(index);
+        int leftChildLeftChildIndex = this.getLeftChildIndex(leftChildIndex);
+        int rightChildLeftChildIndex = this.getRightChildIndex(leftChildIndex);
+        int leftChildRightChildIndex = this.getLeftChildIndex(rightChildIndex);
+        int rightChildRightChildIndex = this.getRightChildIndex(rightChildIndex);
+
+        int currentMaxIndex = leftChildIndex;
+        if(this.less(currentMaxIndex,rightChildIndex)){
+            currentMaxIndex = rightChildIndex;
+        }
+        if(this.less(currentMaxIndex,leftChildLeftChildIndex)){
+            currentMaxIndex = leftChildLeftChildIndex;
+        }
+        if(this.less(currentMaxIndex,rightChildLeftChildIndex)){
+            currentMaxIndex = rightChildLeftChildIndex;
+        }
+        if(this.less(currentMaxIndex,leftChildRightChildIndex)){
+            currentMaxIndex = leftChildRightChildIndex;
+        }
+        if(this.less(currentMaxIndex,rightChildRightChildIndex)){
+            currentMaxIndex = rightChildRightChildIndex;
+        }
+        return currentMaxIndex;
+    }
+
+    private int getLeftChildIndex(int i) {
         return 2 * i;
     }
 
-    public int getRightChildIndex(int i) {
+    private int getRightChildIndex(int i) {
         return (2 * i) + 1;
+    }
+
+    private boolean hasChildren(int i) {
+        return (this.getLeftChildIndex(i) < N && this.getRightChildIndex(i) < N);
     }
 
     public Key getMin() {
@@ -57,23 +128,34 @@ public class MinMaxHeap<Key extends Comparable <? super Key>> {
         return this.pq[i];
     }
 
-    public void set(int i, Key key) {
+    private void set(int i, Key key) {
         this.pq[i] = key;
     }
 
     public Key getMax() {
+        return this.get(this.getMaxIndex());
+    }
+
+    /***
+     *
+     * @return index of the greatest element
+     */
+    private int getMaxIndex() {
         if (this.size() == 0) {
-            return null;
+            throw new EmptyStackException();
         }
 
         if (this.size() == 1) {
-            return this.get(1);
+            return 1;
         }
 
         if (this.size() == 2) {
-            return this.get(2);
+            return 2;
         }
-        return max(this.get(2), this.get(3));
+        if (max(this.get(2), this.get(3)) == this.get(2)) {
+            return 2;
+        }
+        return 3;
     }
 
     private Key max(Key leftValue, Key rightValue) {
@@ -97,48 +179,34 @@ public class MinMaxHeap<Key extends Comparable <? super Key>> {
         return this.get(i).compareTo(this.get(j)) < 0;
     }
 
-
-    /***
-     * Der größere Kindknoten „schwimmt“ nach oben
-     * solange bis er kleiner als der erste Elternknoten ist.
-     * @source AD_2 Sortieren 2 Fol. 31
-     * @param k index of the element
-     */
-    private void swim(int k) {
-        while (k > 1 && less(k / 2, k)) {
-            exch(k / 2, k);
-            k = k / 2;
-        }
-    }
-
     /***
      * Schlüssel hinten an den Heap anfügen
      * und mit dem Schlüssel Größe um 1 erhöhen
      * mit dem Schlüssel nach oben schwimmen
-     * @source AD_2 Sortieren 2 Fol. 30
+     * source: AD_2 Sortieren 2 Fol. 30
      * @param v element which should be inserted
      */
-    public void insert (Key v) {
+    public void insert(Key v) {
         pq[++N] = v;
         pushUp(N);
     }
 
 
     /***
-     * @source https://en.wikipedia.org/wiki/Min-max_heap
+     * source: https://en.wikipedia.org/wiki/Min-max_heap
      * @param i
      */
-    private void pushUp(int i){
-        if(i != 1){
-            if(isIndexMinLevel(i)){
-                if(this.get(i).compareTo(this.get(this.getParentIndex(i)))>=0){
+    private void pushUp(int i) {
+        if (i != 1) {
+            if (isIndexMinLevel(i)) {
+                if (this.get(i).compareTo(this.get(this.getParentIndex(i))) >= 0) {
                     exch(i, this.getParentIndex(i));
                     this.pushUpMax(this.getParentIndex(i));
                 } else {
                     this.pushUpMin(i);
                 }
             } else {
-                if(this.get(i).compareTo(this.get(this.getParentIndex(i)))<0){
+                if (this.get(i).compareTo(this.get(this.getParentIndex(i))) < 0) {
                     exch(i, this.getParentIndex(i));
                     this.pushUpMin(this.getParentIndex(i));
                 } else {
@@ -152,10 +220,10 @@ public class MinMaxHeap<Key extends Comparable <? super Key>> {
      * @source https://en.wikipedia.org/wiki/Min-max_heap
      * @param i
      */
-    private void pushUpMin(int i){
-        if(this.hasGrandparent(i) && this.get(i).compareTo(this.get(this.getGrantparentIndex(i))) <0){
-            exch(i,this.getGrantparentIndex(i));
-            pushUpMin(this.getGrantparentIndex(i));
+    private void pushUpMin(int i) {
+        if (this.hasGrandparent(i) && this.get(i).compareTo(this.get(this.getGrandparentIndex(i))) < 0) {
+            exch(i, this.getGrandparentIndex(i));
+            pushUpMin(this.getGrandparentIndex(i));
         }
     }
 
@@ -163,16 +231,108 @@ public class MinMaxHeap<Key extends Comparable <? super Key>> {
      * @source https://en.wikipedia.org/wiki/Min-max_heap
      * @param i
      */
-    private void pushUpMax(int i){
-        if(this.hasGrandparent(i) && this.get(i).compareTo(this.get(this.getGrantparentIndex(i))) >0){
-            exch(i,this.getGrantparentIndex(i));
-            pushUpMax(this.getGrantparentIndex(i));
+    private void pushUpMax(int i) {
+        if (this.hasGrandparent(i) && this.get(i).compareTo(this.get(this.getGrandparentIndex(i))) > 0) {
+            exch(i, this.getGrandparentIndex(i));
+            pushUpMax(this.getGrandparentIndex(i));
         }
     }
 
+    /***
+     * delete the greatest element
+     */
+    public void delMax() {
+        int index = getMaxIndex();
+        if (index == -1){
+            return ;
+        }
+        Key temp = pq[index];
+        N = N - 1;
+        pq[index] = pq[N];
+        pushDown(index);
+        //this.delete(this.getMaxIndex());
+    }
+
+    private boolean isLastIndex(int index) {
+        return index == N;
+    }
+
+    // https://stackoverflow.com/a/39392866
+    private void delete(int index) {
+        if (isLastIndex(index)) {
+            pq[N] = null;
+            N--;
+
+        } else {
+            int lastIndex = N;
+            System.out.println("last index: "+lastIndex);
+            System.out.println("last index value: "+this.get(lastIndex));
+            exch(index, lastIndex);
+            this.delete(lastIndex);
+            this.pushUp(index);
+            this.pushDown(index);
+            //this.floydBuildHeap();
+        }
+    }
+
+    /***
+     * @source https://en.wikipedia.org/wiki/Min-max_heap
+     * @param index
+     */
+    private void pushDown(int index) {
+        if (isIndexMinLevel(index)) {
+            this.pushDownMin(index);
+        } else {
+            this.pushDownMax(index);
+        }
+    }
+
+    /***
+     * @source https://en.wikipedia.org/wiki/Min-max_heap
+     * @param index
+     */
+    private void pushDownMin(int index) {
+        if (this.hasChildren(index)) {
+            int m = this.getIndexOfSmallestChildOrGrandChild(index);
+            if (this.isGrandChildOf(m, index)) {
+                if (this.less(m, index)) {
+                    this.exch(m, index);
+                    int parentIndexOfM = this.getParentIndex(m);
+                    if (this.less(parentIndexOfM, m)) {
+                        this.exch(m, parentIndexOfM);
+                    }
+                    this.pushDownMin(m);
+                }
+            } else if (this.less(m, index)) {
+                this.exch(m, index);
+            }
+        }
+    }
+
+    /***
+     * @source https://en.wikipedia.org/wiki/Min-max_heap
+     * @param index
+     */
+    private void pushDownMax(int index) {
+        if (this.hasChildren(index)) {
+            int m = this.getIndexOfLargestChildOrGrandChild(index);
+            if (this.isGrandChildOf(m, index)) {
+                if (this.less(index, m)) {
+                    this.exch(m, index);
+                    int parentIndexOfM = this.getParentIndex(m);
+                    if (this.less(m, parentIndexOfM)) {
+                        this.exch(m, parentIndexOfM);
+                    }
+                    this.pushDownMax(m);
+                }
+            } else if (this.less(m, index)) {
+                this.exch(m, index);
+            }
+        }
+    }
 
     @Override
-    public String toString () {
+    public String toString() {
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer.append("digraph G {\n");
         stringBuffer.append(branchToString(1));
@@ -180,21 +340,21 @@ public class MinMaxHeap<Key extends Comparable <? super Key>> {
         return stringBuffer.toString();
     }
 
-    private List<Integer> getChildrenIndexes(int i){
+    private List<Integer> getChildrenIndexes(int i) {
         ArrayList<Integer> childrenIndexes = new ArrayList<>();
         int leftChildrenIndex = this.getLeftChildIndex(i);
-        if(leftChildrenIndex > this.size()-1){
+        if (leftChildrenIndex > this.size() - 1) {
             return childrenIndexes;
         }
-        if(null != this.get(leftChildrenIndex)){
+        if (null != this.get(leftChildrenIndex)) {
             childrenIndexes.add(leftChildrenIndex);
         }
 
         int rightChildrenIndex = this.getRightChildIndex(i);
-        if(rightChildrenIndex > this.size()-1){
+        if (rightChildrenIndex > this.size() - 1) {
             return childrenIndexes;
         }
-        if(null != this.get(rightChildrenIndex)){
+        if (null != this.get(rightChildrenIndex)) {
             childrenIndexes.add(rightChildrenIndex);
         }
         return childrenIndexes;
@@ -202,9 +362,9 @@ public class MinMaxHeap<Key extends Comparable <? super Key>> {
 
     private String branchToString(int i) {
         StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("\""+i+"\""+" [label="+this.get(i)+"]");
+        stringBuffer.append("\"" + i + "\"" + " [label=" + this.get(i) + "]");
         getChildrenIndexes(i).forEach(childIndex -> {
-            stringBuffer.append("\""+i+"\" -> \""+childIndex+"\";\n");
+            stringBuffer.append("\"" + i + "\" -> \"" + childIndex + "\";\n");
         });
         getChildrenIndexes(i).forEach(child -> {
             stringBuffer.append(branchToString(child));
--- src/p2/test/MinMaxHeapTest.java
+++ src/p2/test/MinMaxHeapTest.java
@@ -4,100 +4,49 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import p2.datastructures.MinMaxHeap;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MinMaxHeapTest {
 
-    private MinMaxHeap<Integer> minMaxHeapOfSortedintListLength3;
-    private MinMaxHeap<Integer> minMaxHeapOfSortedintListLength10;
-    private MinMaxHeap<Integer> minMaxHeapOfUnsortedintListLength10;
-    private MinMaxHeap<Integer> minMaxHeapExampleFromWikipediaAndLecture;
-    private MinMaxHeap<Integer> minMaxHeapExampleFromWikipediaAndLectureForInsertion;
+    private MinMaxHeap<Integer> minMaxHeapExampleFromLecture;
 
 
     @BeforeEach
     void SetUp() {
-        this.minMaxHeapOfSortedintListLength3 = new MinMaxHeap<>(List.of(1, 2, 3));
-        this.minMaxHeapOfSortedintListLength10 =
-                new MinMaxHeap<>(List.of(11, 12, 23, 44, 45, 46, 77, 78, 89, 90));
-        this.minMaxHeapOfUnsortedintListLength10 =
-                new MinMaxHeap<>(List.of(3, 4, 0, 2, 1, 6, 8, 9, 7, 5));
-        this.minMaxHeapExampleFromWikipediaAndLecture =
-                new MinMaxHeap<>(List.of(46, 51, 31, 21, 13, 31, 10, 71, 8, 41, 11, 16));
-        this.minMaxHeapExampleFromWikipediaAndLectureForInsertion = new MinMaxHeap<>(14);
+        this.minMaxHeapExampleFromLecture = new MinMaxHeap<>(14);
         Integer[] intarry = {null,8,71,41,31,10,11,16,46,51,31,21,13};
-        this.minMaxHeapExampleFromWikipediaAndLectureForInsertion.pq = intarry;
-        this.minMaxHeapExampleFromWikipediaAndLectureForInsertion.N = 11;
+        this.minMaxHeapExampleFromLecture.pq = intarry;
+        this.minMaxHeapExampleFromLecture.N = 11;
     }
 
     @Test
-    void minMaxHeapOfSortedintListLength3GetMin() {
-        assertEquals(1, minMaxHeapOfSortedintListLength3.getMin());
+    void insertNewSmallestElementWithMinMaxHeapExampleFromLecture() {
+        this.minMaxHeapExampleFromLecture.insert(6);
+        System.out.println(this.minMaxHeapExampleFromLecture);
+        assertEquals(6, minMaxHeapExampleFromLecture.getMin());
     }
 
     @Test
-    void minMaxHeapOfSortedintListLength3GetMax() {
-        System.out.println(this.minMaxHeapOfSortedintListLength3);
-        assertEquals(3, minMaxHeapOfSortedintListLength3.getMax());
-
+    void insertNewGreatestElementWithMinMaxHeapExampleFromLecture() {
+        this.minMaxHeapExampleFromLecture.insert(85);
+        System.out.println(this.minMaxHeapExampleFromLecture);
+        assertEquals(85, minMaxHeapExampleFromLecture.getMax());
     }
 
     @Test
-    void minMaxHeapOfSortedintListLength10GetMin() {
-        assertEquals(11, minMaxHeapOfSortedintListLength10.getMin());
+    void getMinWithMinMaxHeapExampleFromLecture() {
+        assertEquals(8, minMaxHeapExampleFromLecture.getMin());
     }
 
     @Test
-    void minMaxHeapOfSortedintListLength10GetMax() {
-        System.out.println(this.minMaxHeapOfSortedintListLength10);
-        assertEquals(90, minMaxHeapOfSortedintListLength10.getMax());
+    void getMaxWithMinMaxHeapExampleFromLecture() {
+        assertEquals(71, minMaxHeapExampleFromLecture.getMax());
     }
 
     @Test
-    void minMaxHeapExampleFromWikipediaAndLectureGetMax() {
-        System.out.println(this.minMaxHeapExampleFromWikipediaAndLecture);
-        assertEquals(71, minMaxHeapExampleFromWikipediaAndLecture.getMax());
-    }
-
-    @Test
-    void minMaxHeapExampleFromWikipediaAndLectureGetMin() {
-        assertEquals(8, minMaxHeapExampleFromWikipediaAndLecture.getMin());
-    }
-
-    @Test
-    void minMaxHeapExampleFromWikipediaAndLectureForInsertionInsertNewSmallestElement() {
-        this.minMaxHeapExampleFromWikipediaAndLectureForInsertion.insert(6);
-        System.out.println(this.minMaxHeapExampleFromWikipediaAndLectureForInsertion);
-        assertEquals(6, minMaxHeapExampleFromWikipediaAndLectureForInsertion.getMin());
-    }
-
-    @Test
-    void minMaxHeapExampleFromWikipediaAndLectureForInsertionInsertNewGreatestElement() {
-        this.minMaxHeapExampleFromWikipediaAndLectureForInsertion.insert(85);
-        System.out.println(this.minMaxHeapExampleFromWikipediaAndLectureForInsertion);
-        assertEquals(85, minMaxHeapExampleFromWikipediaAndLectureForInsertion.getMax());
-    }
-
-    @Test
-    void minMaxHeapExampleFromWikipediaAndLectureForInsertionGetMin() {
-        assertEquals(8, minMaxHeapExampleFromWikipediaAndLectureForInsertion.getMin());
-    }
-
-    @Test
-    void minMaxHeapExampleFromWikipediaAndLectureForInsertionGetMax() {
-        assertEquals(71, minMaxHeapExampleFromWikipediaAndLectureForInsertion.getMax());
-    }
-
-    @Test
-    void minMaxHeapOfUnsortedintListLength10GetMax() {
-        System.out.println(this.minMaxHeapOfUnsortedintListLength10);
-        assertEquals(9, minMaxHeapOfUnsortedintListLength10.getMax());
-    }
-
-    @Test
-    void minMaxHeapOfUnsortedintListLength10GetMin() {
-        assertEquals(0, minMaxHeapOfUnsortedintListLength10.getMin());
+    void delMaxWithMinMaxHeapExampleFromLecture() {
+        this.minMaxHeapExampleFromLecture.delMax();
+        System.out.println(this.minMaxHeapExampleFromLecture);
+        assertEquals(51, minMaxHeapExampleFromLecture.getMax());
     }
 }
