d4728651430cec27e498d35514c9bc414644e772
ferdinand <ferdinand.trendelenburg@haw-hamburg.de>
2019-11-20 11:59:07 +0100

    added more Comments

--- src/p2/interfaces/IMinMaxHeap.java
+++ src/p2/interfaces/IMinMaxHeap.java
@@ -1,10 +1,40 @@
 package p2.interfaces;
 
 public interface IMinMaxHeap<Key extends Comparable<? super Key>> {
-    void insert(Key k);
-     Key max();
-     Key min();
-     Key delMin();
-     Key delMax();
-     int size();
+
+ /**
+  * methode to implement to insert a Element in the Heap
+  * @param k element (Key)
+  */
+ void insert(Key k);
+
+ /**
+  * method to implement to return the max Key (have to be on the first Level or on the second)
+  * @return max Value(Key) in Heap
+  */
+ Key max();
+
+ /**
+  * method to implement to return the min Key (have to be on the first Level)
+  * @return min Value(Key) in Heap
+  */
+ Key min();
+
+ /**
+  * method to implement to deleting the min Key (have to be on the first Level)
+  * @return min Value(Key) in Heap
+  */
+ Key delMin();
+
+ /**
+  * method to implement to deleting the min Key (have to be on the first Level)
+  * @return min Value(Key) in Heap
+  */
+ Key delMax();
+
+ /**
+  * getting the Sizo of the Heap
+  * @return size of the Heap
+  */
+ int size();
 }
--- src/p2/interfaces/IWarteschlange.java
+++ src/p2/interfaces/IWarteschlange.java
@@ -4,8 +4,16 @@ import java.util.List;
 
 public interface IWarteschlange<E> {
 
+    /**
+     * deletes and returns the first element
+     * @return the deleted Element
+     */
     E pop();
 
+    /**
+     * inserting the item into the Queue
+     * @param item
+     */
     void push(E item);
 
 }
